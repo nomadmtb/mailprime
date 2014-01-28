0556db2f5c1319dcf54c0f076cb7838ef726ddfb
Kyle <kylegluce@gmail.com>
2014-01-27 20:18:01 -0800

    Added basic Models + admin for now.

--- mailprime/mailer/admin.py
+++ mailprime/mailer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mailer.models import Profile, Campaign, Recipient
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Campaign)
+admin.site.register(Recipient)
--- mailprime/mailer/models.py
+++ mailprime/mailer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+	"""This is the class that extends the User Class"""
+	public_email = models.EmailField(max_length=254)
+	agree_terms = models.BooleanField(default=False)
+	user = models.OneToOneField(User)
+
+class Campaign(models.Model):
+	"""This is the campaign class that describes an indivual devivery event"""
+	title = models.CharField(max_length=200)
+	about = models.TextField()
+	created_date = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
+
+class Recipient(models.Model):
+	"""This is the class that will hold contact information"""
+	email = models.EmailField(max_length=254)
+	created_date = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=False)
+	tracking_id = models.CharField(max_length=200)
