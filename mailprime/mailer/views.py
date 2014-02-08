2a3aaeb6b52fac07055f9b73ece624ba519650a3
Kyle <kylegluce@gmail.com>
2014-02-08 13:06:02 -0800

    Started Adding Templates and URLs

--- mailprime/mailer/views.py
+++ mailprime/mailer/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
 
-# Create your views here.
+def index(request):
+	return render_to_response('index.html',	{	"page_title": "Mailpri.me"	})
