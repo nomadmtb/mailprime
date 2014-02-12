de713b45cbb7f387581afe477188dc067750fada
Kyle <kylegluce@gmail.com>
2014-02-11 19:08:27 -0800

    Better Urls

--- mailprime/mailprime/urls.py
+++ mailprime/mailprime/urls.py
@@ -13,12 +13,12 @@ urlpatterns = patterns('',
 	url(r'^logout$', views.logout, name='logout'),
     url(r'^(\w+)/account$', views.user_account, name='user_account'),
     url(r'^(\w+)/campaigns$', views.user_campaigns, name='user_campaigns'),
-    url(r'^(\w+)/(\d+)$', views.user_campaign, name='user_campaign'),
-    url(r'^(\w+)/(\d+)/recipients$', views.user_campaign_recipients, name='user_campaign_recipients'),
-    url(r'^(\w+)/(\d+)/messages$', views.user_campaign_messages, name='user_campaign_messages'),
-    url(r'^(\w+)/(\d+)/(\d+)$', views.user_campaign_message, name='user_campaign_message'),
-    url(r'^(\w+)/(\d+)/(\d+)/events$', views.user_campaign_message_events, name='user_campaign_message_events'),
-    url(r'^(\w+)/(\d+)/(\d+)/(\d+)$', views.user_campaign_message_event, name='user_campaign_message_event'),
+    url(r'^(\w+)/campaign-(\d+)$', views.user_campaign, name='user_campaign'),
+    url(r'^(\w+)/campaign-(\d+)/recipients$', views.user_campaign_recipients, name='user_campaign_recipients'),
+    url(r'^(\w+)/campaign-(\d+)/messages$', views.user_campaign_messages, name='user_campaign_messages'),
+    url(r'^(\w+)/campaign-(\d+)/message-(\d+)$', views.user_campaign_message, name='user_campaign_message'),
+    url(r'^(\w+)/campaign-(\d+)/message-(\d+)/events$', views.user_campaign_message_events, name='user_campaign_message_events'),
+    url(r'^(\w+)/campaign-(\d+)/message-(\d+)/event-(\d+)$', views.user_campaign_message_event, name='user_campaign_message_event'),
 )
 
 # For Development ONLY. Remove when in production...
