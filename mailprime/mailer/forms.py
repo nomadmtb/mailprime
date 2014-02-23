5eedc3211989e697261bee318ae28e849f1f42ab
Kyle <kylegluce@gmail.com>
2014-02-23 12:39:12 -0800

    Styling Campaign-Form

--- mailprime/mailer/forms.py
+++ mailprime/mailer/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from mailer.models import Campaign, Message
 
 # Model Forms for the Application
@@ -7,6 +7,7 @@ class CampaignForm(ModelForm):
 	class Meta:
 		model = Campaign
 		fields = ['title', 'about']
+		widgets = { 'about': Textarea(attrs={'cols': 35, 'rows': 6})}
 
 class MessageForm(ModelForm):
 	class Meta:
