4050c2638c3c249d1b91bbaed8b9356b0c774e47
Kyle <kylegluce@gmail.com>
2014-03-02 17:03:18 -0800

    message building function

--- mailprime/mailer/models.py
+++ mailprime/mailer/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 import string
+import re
 
 # Create your models here.
 
@@ -89,7 +90,11 @@ class Message(models.Model):
 			built_template = string.replace(built_template, "{{ recipient_email }}", recipient.email)
 			built_template = string.replace(built_template, "{{ body }}", self.body)
 			built_template = string.replace(built_template, "{{ link }}", self.link)
-			messages.append(built_template)
+			track_url = "http://127.0.0.1/tracker/visit/{0}/{1}.jpg".format(recipient.tracking_id, self.message_id)
+			built_template = string.replace(built_template, "{{ tracking_link }}", track_url)
+			built_template = re.sub('[\t\r\n]', '', str(built_template))
+			message = {'to': str(recipient.email), 'from': str(seleted_campaign.user.email), 'subject': str(self.title), 'message': built_template}
+			messages.append(message)
 
 		return messages
 
