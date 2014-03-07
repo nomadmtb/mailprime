0e76a483d39868cbaed1c0268ec2b8a6943beaf5
Kyle <kylegluce@gmail.com>
2014-03-07 14:41:01 -0800

    Fixing messages

--- mailprime/mailer/models.py
+++ mailprime/mailer/models.py
@@ -90,7 +90,7 @@ class Message(models.Model):
 			built_template = string.replace(built_template, "{{ recipient_email }}", recipient.email)
 			built_template = string.replace(built_template, "{{ body }}", self.body)
 			built_template = string.replace(built_template, "{{ link }}", self.link)
-			built_template = string.replace(built_template, "{{ date_today }}", (datetime.now().strftime('%Y-%m-%d %H:%M') + " "))
+			built_template = string.replace(built_template, "{{ date_today }}", datetime.now().strftime('%Y-%m-%d %H:%M'))
 			track_url = "http://127.0.0.1/tracker/visit/{0}/{1}.jpg".format(recipient.tracking_id, self.message_id)
 			built_template = string.replace(built_template, "{{ tracking_link }}", track_url)
 			built_template = re.sub('[\t]', '', str(built_template))
