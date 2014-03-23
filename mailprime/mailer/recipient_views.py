8addd124358897c6a13b090e92df389a9d0da1c0
Kyle <kylegluce@gmail.com>
2014-03-23 16:53:54 -0700

    Recipients list, owner check.

--- mailprime/mailer/recipient_views.py
+++ mailprime/mailer/recipient_views.py
@@ -13,7 +13,13 @@ def user_campaign_recipients(request, param_username, param_campaign_pk):
 	page_vars = {"page_title": 'Campaign Recipients'}
 
 	if current_user(request) and request.user.username == param_username:
-		recipients = Recipient.objects.filter(campaign__pk = param_campaign_pk).order_by('email')
+
+		try:
+			campaign = Campaign.objects.get(pk = param_campaign_pk, user=request.user)
+		except Campaign.DoesNotExist:
+			raise Http404
+
+		recipients = Recipient.objects.filter(campaign = campaign).order_by('email')
 		page_vars['recipients'] = recipients
 		return render(request, 'recipient/index.html', page_vars)
 	else:
@@ -24,34 +30,33 @@ def upload_recipients(request, param_username, param_campaign_pk):
 
 def add_recipient(request, param_username, param_campaign_pk):
 	if current_user(request) and request.user.username == param_username:
-		if request.method == "GET":
-			page_vars = {"page_title": "Add Recipient"}
-			page_vars['form'] = RecipientForm()
+		page_vars = {'page_title': 'Add Recipient'}
 
-			try:
-				page_vars['campaign'] = Campaign.objects.get(pk=param_campaign_pk, user=request.user)
-			except Campaign.DoesNotExist:
-				raise Http404
+		try:
+			page_vars['campaign'] = Campaign.objects.get(pk=param_campaign_pk, user=request.user)
+		except Campaign.DoesNotExist:
+			raise Http404
+
+		# User is requesting page
+		if request.method == "GET":
+			page_vars['form'] = RecipientForm()
 
 			csrfContext = RequestContext(request, page_vars)
 			return render(request, 'recipient/add.html', csrfContext)
+
+		# User is uploading from the form
 		elif request.method == "POST":
 			completed_form = RecipientForm(request.POST)
 
 			if completed_form.is_valid():
 				recipient = completed_form.save(commit=False)
 
-				try:
-					campaign = Campaign.objects.get(pk=param_campaign_pk, user=request.user)
-				except Campaign.DoesNotExist:
-					raise Http404
-
-				recipient.campaign = campaign
+				recipient.campaign = page_vars['campaign']
 				recipient.active = True
 
 				# Send out invitation!!!
 				recipient.save()
-				return HttpResponseRedirect('/' + request.user.username + '/campaign-' + str(campaign.pk) + '/recipients')
+				return HttpResponseRedirect('/' + request.user.username + '/campaign-' + str(page_vars['campaign'].pk) + '/recipients')
 			else:
 				generate_form_errors(request, completed_form)
 				page_vars['form'] = completed_form
