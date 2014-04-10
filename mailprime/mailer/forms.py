64a578edb32aa12245d06ad6b353cf58937a6a22
Kyle <kylegluce@gmail.com>
2014-04-10 13:29:47 -0700

    Fixed Message model/modelform to accomidate deploy_hour

--- mailprime/mailer/forms.py
+++ mailprime/mailer/forms.py
@@ -21,43 +21,17 @@ class RecipientForm(forms.ModelForm):
 
 class MessageForm(forms.ModelForm):
 
-	DEPLOY_HOURS = (
-		(0, '12:00 AM'),
-		(1, '1:00 AM'),
-		(2, '2:00 AM'),
-		(3, '3:00 AM'),
-		(4, '4:00 AM'),
-		(5, '5:00 AM'),
-		(6, '6:00 AM'),
-		(7, '7:00 AM'),
-		(8, '8:00 AM'),
-		(9, '9:00 AM'),
-		(10, '10:00 AM'),
-		(11, '11:00 AM'),
-		(12, '12:00 PM'),
-		(13, '1:00 PM'),
-		(14, '2:00 PM'),
-		(15, '3:00 PM'),
-		(16, '4:00 PM'),
-		(17, '5:00 PM'),
-		(18, '6:00 PM'),
-		(19, '7:00 PM'),
-		(20, '8:00 PM'),
-		(21, '9:00 PM'),
-		(22, '10:00 PM'),
-		(23, '11:00 PM'),
-		)
-
-	temp_deploy_hour = forms.ChoiceField(choices=DEPLOY_HOURS, initial=0)
+	# Overriding this field so we can format the output with no time
+	deploy_date = forms.DateTimeField(widget=forms.DateInput(format='%Y-%m-%d'))
 
 	class Meta:
 		model = Message
-		fields = ['title', 'body', 'link', 'template', 'deploy_date']
+		fields = ['title', 'body', 'link', 'template', 'deploy_date', 'deploy_hour']
 		localized_fields = ['__all__']
 
 	def clean(self):
 		cleaned_data = self.cleaned_data
-		deploy_hour = int(cleaned_data['temp_deploy_hour'])
+		deploy_hour = int(cleaned_data['deploy_hour'])
 
 		# Apply deploy-hour to deploy-date
 		cleaned_data['deploy_date'] = cleaned_data['deploy_date'].replace(hour=deploy_hour, minute=0)
